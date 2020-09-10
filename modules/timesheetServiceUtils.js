6216c5105a016a8204e8b5e9cb25061cd7a1ac23
Nick <nick@polyforest.com>
2020-09-10 10:19:38 -0500

    Change to use first folder created by this app, regardless of name

--- modules/timesheetServiceUtils.js
+++ modules/timesheetServiceUtils.js
@@ -42,26 +42,26 @@ export async function createTimesheet(title) {
 									}
 								]
 							},
-							{
-								values: [
-									{
-										userEnteredValue: {
-											stringValue: "1-2-2020"
-										}
-									},
-									{
-										userEnteredValue: {
-											stringValue: "1-3-2020 3:22:34 A.M."
-										}
-									},
-									{
-										userEnteredValue: {
-											stringValue: "4:22"
-										}
-									}
-
-								]
-							}
+							// {
+							// 	values: [
+							// 		{
+							// 			userEnteredValue: {
+							// 				stringValue: "1-2-2020"
+							// 			}
+							// 		},
+							// 		{
+							// 			userEnteredValue: {
+							// 				stringValue: "1-3-2020 3:22:34 A.M."
+							// 			}
+							// 		},
+							// 		{
+							// 			userEnteredValue: {
+							// 				stringValue: "4:22"
+							// 			}
+							// 		}
+							//
+							// 	]
+							// }
 						]
 					}
 				}
@@ -72,8 +72,8 @@ export async function createTimesheet(title) {
 	console.debug("newSheetResponse", newSheetResponse);
 	const sheet0Id = newSheetResponse.result.sheets[0].properties.sheetId;
 
-	// Format header row to be bold:
-	const formatResponse = await gapi.client.sheets.spreadsheets.batchUpdate({
+	// Format header row to be bold and frozen, and the date columns formatted.
+	const formatHeaderResponse = await gapi.client.sheets.spreadsheets.batchUpdate({
 		spreadsheetId: newSheetResponse.result.spreadsheetId,
 		requests: [
 			{
@@ -114,12 +114,11 @@ export async function createTimesheet(title) {
 					cell: {
 						userEnteredFormat: {
 							numberFormat: {
-								type: "DATE_TIME",
-								pattern: "yyyy-mm-dd h:mm"
+								type: "DATE_TIME"
 							}
 						}
 					},
-					fields: "*"
+					fields: "userEnteredFormat.numberFormat"
 				}
 			},
 			{
@@ -133,8 +132,8 @@ export async function createTimesheet(title) {
 					cell: {
 						userEnteredFormat: {
 							numberFormat: {
-								type: "DATE_TIME",
-								pattern: "[hh]:[mm]"
+								type: "TIME",
+								format: "h:mm"
 							}
 						}
 					},
@@ -143,8 +142,9 @@ export async function createTimesheet(title) {
 			}
 		]
 	});
-	console.debug("formatResponse", formatResponse);
+	console.debug("formatResponse", formatHeaderResponse);
 
+	// Add the new spreadsheet to the 'Timesheets' folder:
 	const timesheetFolder = await getTimesheetFolderId();
 	const fileUpdateResponse = await gapi.client.drive.files.update({
 		fileId: newSheetResponse.result.spreadsheetId,
@@ -155,12 +155,15 @@ export async function createTimesheet(title) {
 }
 
 /**
- * Creates or returns the timesheets folder.
+ * Returns the first folder created by this application.
+ * Creates a new folder named "Timesheets" if none exists.
+ * This folder may be moved or renamed in Google Drive.
+ *
  * @return Promise<String>
  */
 export async function getTimesheetFolderId() {
 	const response = await gapi.client.drive.files.list({
-		"q": "name = 'Timesheets' and mimeType = 'application/vnd.google-apps.folder' and trashed = false"
+		"q": "mimeType = 'application/vnd.google-apps.folder' and trashed = false"
 	})
 	if (response.result.files.length > 0) return response.result.files[0].id;
 
@@ -191,17 +194,16 @@ export async function appendTimeEntry(spreadsheetId, startDate, endDate, group, 
 
 		// // The A1 notation of a range to search for a logical table of data.
 		// // Values are appended after the last row of the table.
-		range: 'A1:E1',
+		range: 'B1:E1',
 
 		// How the input data should be interpreted.
-		valueInputOption: 'USER_ENTERED',
+		valueInputOption: 'RAW',
 
 		resource: {
 			values: [
 				[
-					startDate,
-					endDate,
-					"",
+					0.5,
+					"=C1-B1",
 					group,
 					comment
 				]
